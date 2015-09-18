a5b3dfceaf901dbac0ccbda51edb1edc0b03ce8d
cssru <cssru@mail.ru>
2015-09-18 17:15:41 +0300

    Duplicate Dao Bean removed

--- src/main/java/test/config/ServiceConfig.java
+++ src/main/java/test/config/ServiceConfig.java
@@ -2,21 +2,15 @@ package test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import test.dao.HomoDao;
-import test.dao.impl.HomoDaoImpl;
 import test.service.HomoService;
 import test.service.impl.HomoServiceImpl;
 
 @Configuration
 public class ServiceConfig {
+
     @Bean
     public HomoService homoService() {
         return new HomoServiceImpl();
-    }
-
-    @Bean
-    public HomoDao homoDao() {
-        return new HomoDaoImpl();
     }
 
 }
