0accf87dc8eb56fe0a13c6be0c6308546eeadfb8
cssru <cssru@mail.ru>
2015-09-21 12:20:18 +0300

    Removed useless authorization check

--- src/main/java/test/web/PersonController.java
+++ src/main/java/test/web/PersonController.java
@@ -1,7 +1,6 @@
 package test.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,10 +23,6 @@ public class PersonController {
     public String addHomo(@Valid PersonDto personDto, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
-            return "index";
-        }
-
-        if (!SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
             return "index";
         }
 
